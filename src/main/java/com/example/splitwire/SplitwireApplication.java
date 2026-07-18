d5baab80b4741124f5913af0efd8f563f2c0f739
arshadansari7 <arshadansarivajrala@gmail.com>
2026-07-18 12:56:00 +0530

    Spring Boot project setup : initial commit

---
+++ src/main/java/com/example/splitwire/SplitwireApplication.java
@@ -0,0 +1,15 @@
+package com.example.splitwire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitwireApplication {
+
+
+    public static void main(String[] args) {
+        System.out.println("SPLITWIRE - ARSHAD");
+        SpringApplication.run(SplitwireApplication.class, args);
+    }
+
+}
